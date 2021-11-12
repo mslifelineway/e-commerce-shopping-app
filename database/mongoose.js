609fb68d7bbfd30460a5d4821b22626beb19b310
= <=>
2021-11-12 17:20:04 +0530

    stripe payment successfull complete code

---
+++ database/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const url = `mongodb://${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const rules = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(url, rules, function (err) {
+  if (err) {
+    console.log(
+      "\n\tDatabase connection failed! Please start the mongodb server. \nError : ",
+      err
+    );
+    console.log("--------------------------------------------------");
+    process.exit(1);
+  } else {
+    console.log(
+      "\n\t",
+      mongoose.connection.readyState,
+      "DB Connection Successfully"
+    );
+    console.log("--------------------------------------------------");
+  }
+});
